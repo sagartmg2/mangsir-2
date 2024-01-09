53a1acc0a2591f0a8374a4a8df388474c12815e1
Sagar <dev.sagartmg@gmail.com>
2024-01-09 15:57:58 +0545

    nested-array-objects

--- index.js
+++ index.js
@@ -1,123 +1,100 @@
-let count = 100;
-count = 20000;
-
-/* array
-    - elements
-    - indexes  // always starts with 0 
-*/
-// let images = ["https://ur1", "https://ur2"];
-// console.log(images);
-// console.log("before", images[0]);
-// images[0] = "https://domain/image.png";
-// images[1] = "https://domain/images-second.png";
-// console.log("after", images[0]);
-// console.log(images[1]);
-// console.log(images);
-
-// let course = ['mern', 'qa']
-// let courseStartTime = [3, 4]
-// let courseEndTime = [4, 5]
-// let courseDurationMonth = [3, 2]
-// let courseStatus = [true, false]
-
-// let courseInfo= ['courses are:',course,'courses starting time are:' ,courseStartTime,'course end time are:' ,courseEndTime,'course duration are:',courseDurationMonth, 'course status are:',courseStatus]
-// console.log(courseInfo)
-
-// let Coursess = [["mern","starttime","3","endtime","4","3months",true],["js","3-5","4months",false],["python","4-5","6months",true]]
-// console.log(Coursess);
-
-let course1 = "mern";
-let course1StartTime = 3;
-let course1EndTime = 4;
-let course1DurationInMonths = 3;
-let course1Status = true;
-
-let course2 = "qa";
-let course2StartTime = 4;
-let course2EndTime = 5;
-let course2DurationinMonths = 2;
-let course2Status = false;
-
-/* object 
-    let <objectName> = {
+/* object
+    
+    let <objectVaraible> = {
         <key>:<value>,
         <key>:<value>,
-        <attribute>:<value>,
         <property>:<value>,
+        <attribute>:<value>,
     }
-
-    - singular variable name
-    - keys are usually smallCase
-    - must have comma after values
-
 */
 
-let courses = ["qa", "pytho", "mern"];
-
-let projector1 = {
-  color: "white",
-  price: 50000,
-  brand: "viewsonic",
-};
-
-let projector2 = {
-  color: "black",
-  price: 50000,
+let myMobile = {
   brand: "samsung",
+  model: "s20",
+  color: "white",
 };
 
-let course_1 = {
-  title: "mern",
-  startTime: 3,
-  endTime: 4,
-  duration: "1 hour",
-  status: true,
-};
-course_1.title = "mern-stack";
-/* code here.. */
-
-let course_2 = {
-  startTime: 3,
-  endTime: 4,
-  duration: "1 hour",
-  status: true,
-  title: "quality asssurance",
+let myFriendMobile = {
+  brand: "apple",
+  model: "s20",
+  color: "white",
 };
 
-courses = [course_1, course_2];
-console.log(course_2);
+// let mobiles = [myMobile, myFriendMobile];
 
-let room1 = {
-  name: "living",
-  color: "green",
-  size: "200sq feet",
-};
-
-let room2 = {
-  name: "ktch", // change this to dining
-  color: "blue",
-  size: "200 sq feet",
-};
-
-console.log("before - room2 name", room2.name);
-room2.name = "dining";
-console.log("after - room2 name", room2.name);
-
-// let rooms = [room1,room2];
-
-let rooms = ["living", "kitchen", "bedroom"];
-
-rooms = [
+let mobiles = [
   {
-    room: "living",
-    color: "lime",
-    dimensionInSqFoot: 10,
+    user: "ram",
+    brand: {
+      name: "samsiung",
+      url: "https://www.samsung.com",
+    },
   },
   {
-    room: "ktch", // change this to dining
-    color: "white",
-    dimensionInSqFoot: 20,
+    user: "hari",
+    brand: "app", // apple
   },
 ];
-/* code here.. */
-console.log(rooms);
+
+// let harisMobile = {
+//     user:"hari",
+//     brand:"app" // apple
+// }
+// let harisMobile = mobiles[1]
+// console.log(harisMobile.brand);
+// harisMobile.brand = "apple"
+// console.log(harisMobile.brand);
+
+mobiles[1].brand = "apple";
+console.log(mobiles);
+
+let user1 = {
+  firstName: "ram",
+  middleName: "bdr", // change to bahadur via code
+  address: {
+    street: "kumari-galli",
+    ward: 10,
+    province: "3", // change to bagmati via code
+  },
+  phones: [
+    {
+      provider: "ntc",
+      number: "984XXX", // change to 9840425511 via code
+      activatedDate: "2022",
+    },
+    {
+      provider: "ncell",
+      number: "984XXX",
+      activatedDate: "2023",
+    },
+  ],
+};
+
+/* code here */
+user1.middleName = "bahadur";
+user1.address.province = "bagmati";
+user1.phones[0].number = "984510234";
+user1.phones[1].number = "980210234";
+console.log(user1.phones[0].number);
+console.log(user1);
+
+let todosApiData = {
+  data: {
+    todos: [
+      { title: "html", status: "completed" },
+      { title: "css", status: "completed" },
+      { title: "js", status: "completed" }, // TODO: change this status to pending
+    ],
+  },
+};
+
+todosApiData.data.todos[2].status = "pending";
+
+/* code here */
+/* html is completed */
+/* css is completed */
+/* js is pending */
+        
+/* research: function  */
+
+
